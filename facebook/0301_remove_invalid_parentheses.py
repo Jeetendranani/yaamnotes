7ad5dd423091a700260169eb8f07608472deaa9f
Randy Li <yunpeng-li@hotmail.com>
2018-03-28 20:08:12 -0700

    Add more built-in function knowledgy

--- facebook/0301_remove_invalid_parentheses.py
+++ facebook/0301_remove_invalid_parentheses.py
@@ -490,5 +490,16 @@ class C:
     
     The returned property object also has the attributes fget, fset, gdel corresponding to the constructor arguments.
     
-
+range(stop)
+    range(start, stop[, step])
+    Rather than being a function, range is actually an immutable sequence type, as documented in Ranges and Sequence 
+    Types - list, tuple, range.
+    
+repr(object)
+    Return a string containning a printable representation of an object, For many types, this function makes an attempt 
+    to return a string that would yield an object with the same value when passed to eval(), otherwise the 
+    representation is a string enclosed in angle brackets that contains the name of the type of the object together
+    with additional information often including the name and address of the object. A class can control what this 
+    function returns for its instances by defining a __repr__() method.
+-
 """
