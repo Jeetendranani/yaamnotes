0b46bfb129f98a172e7503ca317fc6d7831dbaf4
Randy Li <yunpeng-li@hotmail.com>
2018-04-10 13:18:48 -0700

    facebook products

---
+++ facebook/facebook_product.py
@@ -0,0 +1,33 @@
+"""
+Facebook
+
+Ads interfaces
+
+We build the products that advertisers use to create and manage ads on Facebook. We define success by making the
+business that use our tolls more effective, which creates value for both businesses and people who use Facebook. We
+are responsible for the entire front-end stack, from the database layer and core APIs to the most advanced JavaScript
+based interfaces in the company. We pride ourselves on bing early adopters of new technology, which helps us handle
+the incredible scaling challenges presented by advertisers. Recent examples include building the first React & Flux
+based applications and now the first native mobile interfaces built entirely atop React Native.
+
+Ads Delivery
+
+The ads delivery team's goal is to help match people with the ads that are most likely to be relevant useful to them.
+More relevant ads not only are a better experience for people but also deliver better results for advertisers since
+they are bing shown to people who are most likely to be interested. In terms of technology, this team is responsible for
+the infrastructure, machine learning, and optimization systems that determine which ads to deliver. Other key
+responsibility include the auction logic for pricing ads in a way tht creates the right incentives for advertisers and
+our integrity systems for preventing bad ads from bing shown to people. This team also looks to solve the tough product
+challenge of delivering ads in a way that is predictable and intuitive for all types of advertisers, including those
+with varying levels of sophistication.
+
+AdTech
+
+The adTech team is working to take the sophisticated software stack Facebook has built for its own use over the past 5
+years and making it available to the world. We are combining Facebook machine-learning and optimization know-how with
+our understanding of real people to provide advertisers and publishers (app developers, website owners, video producers)
+with the ability to have higher quality advertising facilities. In turn, this creates a win-win-win situation where
+everyone -- people, advertisers and publishers - get more out of their advertising. We work across the full stack and
+are looking to hire systems and product engineers who are passionate about scaling and making a real impact on the
+world.
+"""
