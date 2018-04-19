6adcd5d8ffd4e357f2bc45b13c06a517d02e2841
Randy Li <yunpeng-li@hotmail.com>
2018-04-18 22:35:21 -0700

    stringio and bytesio

---
+++ python/stringio_and_bytesio.py
@@ -0,0 +1,20 @@
+from io import StringIO, BytesIO
+
+
+f = StringIO()
+f.write('hello')
+f.write(' ')
+f.write('world!')
+print(f.getvalue())
+
+f = StringIO("Hello!\nHi!\nGoodbye!")
+
+while True:
+    s = f.readline()
+    if s == '':
+        break
+    print(s.strip())
+
+f = BytesIO()
+f.write('中文'.encode('utf-8'))
+print(f.getvalue())
