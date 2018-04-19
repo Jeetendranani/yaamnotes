ddb849c9e7180e406bf09bab949eaa53c37cf668
Randy Li <yunpeng-li@hotmail.com>
2018-04-19 11:44:57 -0700

    pickling

---
+++ python/pickling.py
@@ -0,0 +1,11 @@
+import pickle
+
+d = dict(name="Randy", age=37, role="QA")
+
+with open('dumps.txt', 'wb') as f:
+    pickle.dump(d, f)
+
+with open('/Users/yunpengli/PycharmProjects/leetcode/dumps.txt', 'rb') as f:
+    nd = pickle.load(f)
+
+print(nd)
