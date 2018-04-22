aa1b8b2455dff65ed186c3df8083d2f7fe7b709e
Randy Li <yunpeng-li@hotmail.com>
2018-04-22 13:25:59 -0700

    system design outline

---
+++ system_design/1_5_2_business_process.py
@@ -0,0 +1,69 @@
+"""
+business process modeling notation (BPMN)
+    - events
+    - processes
+    - work flows
+    todo: something about microsoft visio
+    todo: something about case tools such as visible analyst
+    todo: something about online diagramming tools such as draw
+
+
+1.8 systems development tools
+    todo: modeling tools
+    todo: prototyping tools
+    todo: computer aided system engineering tools
+    todo: something about visible systems corporation, a popular case tool
+
+1.9 system development methods
+    todo: structured analysis
+    todo: object oriented analysis
+    todo: agile methods / adaptive methods
+
+    system analysis: the purpose of the system analysis phase is to built a logical model fo the new system. the first
+    step is requirements modeling, where the analyst investigates business processes and documents what the new system
+    must do to satisfy users. requirements modeling continues the investigation that began during the systems planing
+    phase. to understand the system, fact finding using techniques such as interviews, surveys, document review,
+    observation, and sampling is performed. the fact finding results are used to build business models, data and process
+    models, and object models.
+    the deliverable for the systems analysis phase is the system requirements document. the system requirements document
+    describes management and user requirement, costs and benefits, and outlines alternative development strategies.
+
+    system design: the purpose of the system deign phase is to create a physical model that will satisfy all documented
+    requirements for the system. At this stage, the user interface is designed and necessary outputs, inputs, and
+    processes are identified. in addition internal and external controls are designed and designed, including computer
+    based and manual features to guarantee that the system will be reliable, accurate, maintainable, and secure.
+    during the systems design phase, the application architecture is also determined, which programmers will use to
+    transform the logical design into program modules and code.
+
+    the deliverable for this phase is the system design specification, which is presented to management and users for
+    review and approval. Management and user involvement is critical to avoid any misunderstanding about what the new
+    system will do, how it will do it, and what it will cost.
+
+    system implementation: during the systems implementation phase, the new system is constructed. whether the
+    developers use structured analysis or oo methods, the procedure is teh same programs are written, tested and
+    documented, and the system is installed. if the system was purchased as a package, system analysts configure the
+    software and perform any necessary modifications, the objective of systems implementation phase is to deliver a
+    completely functioning and documented information system. at the conclusion of this phase, the system is ready to
+    use. final preparations include converting data to the new system's file, training users, and performing the
+    actual transition to the new system.
+
+    todo: chapter 2, learn use cases
+    todo: chapter 3, manage system project
+
+system analysis
+
+    todo: chapter 4, requirements modeling
+    todo: chapter 5, data process modeling
+    todo: chapter 6, object modeling
+    todo: chapter 7, development strategies
+
+
+system design
+
+    todo: chapter 8, user interface design
+    todo: chapter 9, data design
+    todo: chapter 10, system architecture
+
+
+
+"""
