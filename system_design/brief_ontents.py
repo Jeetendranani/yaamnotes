2417645d96b91d502decff8afcaa6b4435d43e14
Randy Li <yunpeng-li@hotmail.com>
2018-04-22 02:03:46 -0700

    system analysis and design

---
+++ system_design/brief_ontents.py
@@ -0,0 +1,448 @@
+"""
+Brief contents
+
+phase 1: system planning
+    chapter 1 introduction to system analysis and design
+    chapter 2 analyzing hte business case
+    chapter 3 managing system projects
+
+phase 2: system analysis
+    chapter 4 requirements modeling
+    chapter 5 data and process modeling
+    chapter 6 object modeling
+    chapter 7 development strategies
+
+phase 3: system design
+    chapter 8 user interface design
+    chapter 9 data design
+    chapter 10 system architecture
+
+phase 4: system implementation
+    chapter 11 managing system implementation
+
+phase 5: system support and security
+    chapter 12 managing system support and security
+
+The system analyst's toolkit
+    A communication tools
+    case tools
+    financial analysis tools
+    internet resource tools
+
+
+table of contents
+
+phase 1: system planning
+
+chapter 1
+introduction to system analysis and design
+
+learning objectives
+1.1 introduction
+1.2 what is information technology?
+    1.2.1 the changing nature of information technology
+    1.2.2 system analysis and design
+    1.2.3 what does a system analyst do?
+case in point 1.1: could nine financial advisors
+1.3 information system components
+    1.3.1 hardware
+    1.3.2 software
+    1.3.3 data
+    1.3.4 process
+    1.3.5 people
+1.4 Business today
+    1.4.1 the internet model
+    1.4.2 B2C (Business to consumer)
+    1.4.3 B2B (business to business)
+1.5 Modeling business operations
+    1.5.1 business profiles
+    1.5.2 business process
+1.6 Business information system
+    1.6.1 enterprise computing
+    1.6.2 transaction processing
+    1.6.3 business support
+    1.6.4 knowledge management
+    1.6.5 user productivity
+    1.6.6 system integration
+1.7 what information do user need?
+    1.7.1 top managers
+    1.7.2 middle managers and knowledge workers
+    1.7.3 supervisors and team leaders
+    1.7.4 operational employees
+1.8 systems development tools
+    1.8.1 modeling
+    1.8.2 prototyping
+    1.8.3 computer aided system engineering tools
+1.9. systems development methods
+    1.9.1 structured analysis
+    1.9.2 object-oriented analysis
+    1.9.3 agile methods
+    1.9.4 other development methods
+1.10 the information technology department
+    1.10.1 application development
+case in point 1.2: global hotels and momma's motels
+    1.10.2 system support and security
+    1.10.3 user support
+    1.10.4 database administration
+    1.10.5 network administration
+    1.11.6 web support
+    1.10.7 quality assurance
+case in point 1.3: what should lisa do?
+1.11 the system analyst
+    1.11.1 role
+    1.11.2 knowledge, skill and education
+    1.11.3 certification
+    1.11.4 career opportunities
+case in point 1.4: just in time airfeight, inc.
+1.12 trends in information technology
+a question of ethics
+1.13 chapter summary
+key terms
+chapter exercises
+
+chapter 2
+analyzing hte business case
+learning objectives
+2.1 introduction
+2.2 a framework for it system development
+    2.2.1 strategic planning overview
+    2.2.2 what is swot analysis?
+    2.2.3 strategic planning for it projects
+case in point 2.1: Lo Carb Meals
+    2.2.4 the changing role of the it department
+case in point 2.2: attaway airline, part one
+2.3 what is a business case?
+2.4 information system projects
+    2.4.1 main reasons for systems project
+case in point 2.3: trent college
+    2.4.2 factors tht affect system projects
+    2.4.3 internal factors
+    2.4.4 external factors
+2.5 evaluation of systems requirements
+    2.5.1 systems requests forms
+    2.5.2 system review comments
+2.6 overview of feasibility
+    2.6.1 operational feasibility
+    2.6.2 economic feasibility
+    2.6.3 technical feasibility
+    2.6.4 schedule feasibility
+2.7 evaluating feasibility
+2.8 setting priorities
+    2.8.1 factors that affect priority
+    2.8.2 discretionary and non-discretionary projects
+case in point 2.4: attaway airlines, part two
+2.9 preliminary investigation overview
+    2.9.1 interaction with managers, users, and other stakeholders
+    2.9.2 planning the preliminary investigation
+a question of ethics
+2. 10 chapter summary
+key terms
+chapter exercises
+
+chapter 3
+managing systems projects
+
+learning objectives
+3.1 introduction
+3.2 overview of project management
+    3.2.1 what shapes a project?
+    3.2.2 what is a project triangle
+    3.2.3 what does a project manager do?
+3.3 creating a work breakdown structure
+    3.3.1 gantt charts
+    3.3.2 pert/cpm chats
+    3.3.3 identifying tasks in a work breakdown structure
+case in point 3.1: parallel services
+    3.3.4 factors affecting duration
+case in point 3.2: sunrise software
+    3.3.5 displaying the work breakdown structure
+3.4 Identifying tasks patterns
+    3.4.1 task patterns
+    3.4.2 using task boxes to create a model
+    3.4.3 task patterns
+    3.4.4 identifying task patterns
+    3.4.5 working with complex task patterns
+3.5 calculating the critical path
+    3.5.1 critical path
+    3.5.2 calculating the critical path
+3.6 project monitoring and control
+    3.6.1 monitoring and control techniques
+    3.6.2 maintaining a schedule
+3.7 reporting
+    3.7.1 project status meetings
+    3.7.2 project status reports
+3.8 project management examples
+    3.8.1 pert/cpm examples
+3.9 project management software
+case in point 3.3: census 2010
+3.10 risk management
+    3.10.1 steps in risk management
+    3.10.2 risk management software
+3.11 managing for success
+    3.11.1 business issue
+    3.11.2 budget issue
+    3.11.3 schedule issues
+case in point 3.4: spring forward products
+3.12 the bottom line
+A question of ethics
+3.13 chapter summary
+key terms
+chapter exercises
+
+phase 2:
+system analysis
+
+chapter 4
+requirements modeling
+
+learning objectives
+4.1 introduction
+4.2 system analysis phase overview
+    4.2.1 system analysis activities
+    4.2.2 system analysis skill
+    4.2.3 team-based techniques: jad, rad, and agile methods
+4.3 joint application development
+    4.3.1 user involvement
+    4.3.2 jad participants and roles
+    4.3.3 jad advantages and disadvantages
+4.4 rapid application development
+    4.4.1 rad phases and activities
+    4.4.2 rad objectives
+    4.4.3 rad advantages and disadvantages
+4.5 agile methods
+    4.5.1 agile method advantages and disadvantages
+case in point 4.1: north hills college
+4.6 modeling tools and techniques
+    4.6.1 functional decomposition diagrams
+    4.6.2 business process modeling
+    4.6.3 data flow diagrams
+    4.6.4 unified modeling language
+4.7 system requirements checklist
+    4.7.1 output examples
+    4.7.2 input examples
+    4.7.3 process examples
+    4.7.4 performance examples
+    4.7.5 control examples
+4.8 future growth, costs, and benefits
+    4.8.1 scalability
+    4.8.2 total cost of ownership
+4.9 fact-finding
+    4.9.1 fact-finding overview
+    4.9.2 who, what, where, when, how and why
+    4.9.3 the zachman framework
+4.10 interviews
+case in point 4.2: deep river college
+    4.10.1 unsuccessful interviews
+case in point 4.3: fastpak overnight package system
+4.11 other fact-finding techniques
+    4.11.1 document review
+    4.11.2 observation
+    4.11.3 questionnarires and surveys
+    4.11.5 brainstorming
+    4.11.6 sampling
+    4.11.7 research
+case in point 4.4: cyberstuff
+4.12 documentation
+    4.12.1 the need for recording the facts
+    4.12.2 software tools
+4.13 information management software
+4.14 preview of logical modeling
+a question of ethics
+4.15 chapter summary
+key terms
+chapter exercise
+
+chapter 5
+data and process modeling
+
+learn objectives
+5.1 introduction
+5.2 overview of data and processing modeling tools
+5.3 data flow diagrams
+    5.3.1 dfd symbols
+5.4 creating a set of dfds
+5.5 guidelines for drawing dfds
+case in point 5.1: big ten university
+5.6 data dictionary
+    5.6.1 using case tools for documentation
+    5.6.2 documenting the data elements
+    5.6.3 documenting the data flows
+    5.6.4 documenting the data sores
+    5.6.5 documenting the process
+    5.6.6 documenting the entities
+    5.6.7 documenting the records
+    5.6.8 documenting the reports
+5.7 process description tools
+    5.7.1 modular design
+    5.7.2 structured english
+    5.7.3 design tables
+case in point 5.2: rock solid outfitters (part 1)
+    5.7.4 decision trees
+case in points 5.3: rock solid outfitters (part 2)
+5.8 logical versus physical models
+    5.8.1 sequence of models
+    5.8.2 four model approach
+case in point 5.4: tip top staffing
+a question of ethics
+5.9 chapter summary
+key terms
+chapter exercise
+
+chapter 6
+object modeling
+
+learning objectives
+6.1 introduction
+6.2 overview of object oriented analysis
+    6.2.1 object oriented terms and concepts
+    6.2.2 objects
+    6.2.3 attributes
+    6.2.4 methods
+    6.2.5 messages
+    6.2.6 classes
+6.3 relationships among objects and classes
+    6.3.1 object relationships diagram
+6.4 object modeling with the unified modeling language
+    6.4.1 use case modeling
+case in point 6.1: hiltop motors
+    6.4.2 use case diagrams
+    6.4.3 class diagrams
+case in point 6.2: train the trainers, inc
+    6.4.4 sequence diagrams
+    6.4.5 state transition diagrams
+    6.4.6 activity diagrams
+case in point 6.3: travelbiz
+    6.4.7 business process modeling
+    6.4.8 case tools
+6.5 organizing the object model
+case in point 6.4: cyber associates
+a question of ethics
+6.6 chapter summary
+key terms
+chapter exercise
+
+chapter 7
+development strategies
+
+learning objectives
+7.1 introduction
+7.2 development strategies overview
+7.3 the impact of the internet
+    7.3.1 software as service
+    7.3.2 traditional vs web based system development
+    7.3.3 evolving trends: web 2.0, cloud computing, and mobile devices
+7.4 outsourcing
+    7.4.1 the growth of outsourcing
+    7.4.2 outsourcing fees
+    7.4.3 outsourcing issues and concerns
+    7.4.4 offshore outsourcing
+case in point 7.1: turnkey services
+7.5 in house software development option
+    7.5.1 make or buy decision
+    7.5.2 developing software in house
+    7.5.3 purchasing a software package
+    7.5.4 customizing a software package
+    7.5.5 creating user application
+7.6 the system analyst's role
+case in point 7.2: sterling associates
+7.7 analyzing cost and benefits
+    7.7.1 financial analysis tools
+    7.7.2 cost benefit analysis checklist
+7.8 the software acquisition process
+    step1: evaluate the information system requirements
+    step2: identify potential vendors or outsourcing options
+    step3: evaluate the alternatives
+    step4: perform cost-benefit analysis
+    step5: prepare a recommendation
+    step6: implement the solution
+case in point 7.3: Doug's sporting goods
+7.9 completion of systems analysis tasks
+    7.9.1 system requirements document
+    7.9.2 presentation of management
+7.10 transition of system design
+    7.10.1 preparing for system design
+    7.10.2 logical and physical design
+case in point 7.4: downtown!
+a question of ethics
+7.11 chapter summary
+key terms
+chapter exercises
+
+phase 3: system design
+
+chapter 8
+user interface design
+
+learning objectives
+8.1 introduction
+8.2 systems design phase overview
+    8.2.1 will it succeed?
+8.3 chapter overview
+8.4 what is a user interface?
+    8.4.1 human computer interface
+case in point 8.1: casual observer software
+8.5 seven habits of successful interface designers
+    8.5.1 understand the business
+    8.5.2 maximize graphical effectiveness
+    8.5.3 think like a user
+    8.5.4 use models and prototypes
+    8.5.5 focus on usability
+    8.5.6 invite feedback
+    8.5.7 document everything
+8.6 guidelines for user interface design
+    8.6.1 create an interface that is easy to learn and use
+    8.6.2 enhance user productivity
+    8.6.3 provide users with help and feedback
+    8.6.4 create an attractive layout and design
+    8.6.5 enhance the interface
+    8.6.6 focus on data entry screens
+    8.6.7 use validation rules
+    8.6.8 reduce input volume
+case in point 8.2: boolean toys
+8.7 source document and form design
+8.8 printed output
+    8.8.1 overview of report design
+    8.8.2 types of reports
+    8.8.3 user involvement
+    8.8.4 report design principles
+case in point 8.3: lazy eddie
+case in point 8.4: trustworthy insurance company
+8.9 technology issues
+    8.9.1 output technology
+    8.9.2 input technology
+8.10 security and control issues
+    8.10.1 output security and control
+    8.10.2 input security and control
+8.11 where do we go form here!
+    8.11.1 modular design
+    8.11.2 prototyping
+a question of ethics
+8.12 chapter summary
+key terms
+chapter exercises
+
+chapter 9
+data design
+
+learning objectives
+9.1 introduction
+9.2 data design concepts
+    9.2.1 data structures
+    9.2.2 mario and danica: a data design example
+    9.2.3 is file processing still important?
+    9.2.4 the database environment
+9.3 dbms components
+    9.3.1 interfaces for user, databases administrators and related systems
+    9.3.2 data manipulation language
+    9.3.3 schema
+    9.3.4 physical data repository
+9.4 web based design
+    9.4.1 connecting to the web
+    9.4.2 data security
+9.5 data design terms
+    9.5.1 definitions
+    9.5.2 key fields
+    9.5.3 referential integrity
+9.6 entity relationship diagrams
+    9.6.1
+"""
