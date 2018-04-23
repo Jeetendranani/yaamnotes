54a4bc19f72630d5d00f5172e89d72f52a14f519
Randy Li <yunpeng-li@hotmail.com>
2018-04-23 14:39:57 -0700

    problem solving

---
+++ problem_solving/problem_solving_101.py
@@ -0,0 +1,30 @@
+"""
+Why problem solving?
+
+- Make decision
+
+    - Set goal
+    - face challenges
+    - try to overcome them
+
+    there a fundamental approach to solve these real-life problems, one that can consistently lead you to effective and
+    satisfying solutions.
+
+    Being a problem solver isn't just an ability; it's a whole mind-set, one that drives people to bring out the best
+    in themselves and to shape the world in a positive way. Rather than accepting the status quo, ture problem solvers
+    are constantly trying to proactively shape their environment.
+
+what a problem solving?
+
+Problem solving is a process that can be broken down into four steps:
+    1. Understand the current situation
+    2. identify the root cause of the problem
+    3. Develop an effective action plan
+    4. execute until the problem is solved
+
+problem solving tool box: logic tree
+
+List all the potential root causes of the problem
+
+problem solving tool box: yes / no tree
+"""
