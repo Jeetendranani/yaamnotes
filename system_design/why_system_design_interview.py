a567ce3bd6cad796869fb0f15bc23b53f42fa065
Randy Li <yunpeng-li@hotmail.com>
2018-05-15 16:19:49 -0700

    why system design interview

---
+++ system_design/why_system_design_interview.py
@@ -0,0 +1,31 @@
+"""
+Why system design interviews?
+
+1. What are system design interviews?
+    In system design interviews, candidates are required to show their ability to develop a high-level architecture of
+    a large system. Designing software systems is a very broad topic and even a software engineer having years of
+    experience at a top software company may not claim to be an expert on system design. During such interviews,
+    candidates have 30 to 40 minutes to answer questions like, "How to design a cloud storage system like dropbox?" or
+    "how to design a search engine" etc. In real life, companies spend not weeks but months and hire a big team of
+    software engineers to build such systems. Given this, then how can a person answer such a question in 40 minutes?
+    Moreover, there is no set pattern of such questions. Questions are flexible, unpredictable, usually open-ended, and
+    have no standard or squarely correct answer.
+
+    unlike coding interviews where problem solving ability of the candidates is evaluated, design interviews consist of
+    complicated and fuzzy questions which aim at testing the candidate's ability to analyze a vague and complicated
+    problem, their compatibility with building large systems and how do they present their resolution. Such interviews
+    also look into a candidate's competence in guiding and moving the conversation forward.
+
+    these days, companies are least bothered about your pedigree, where you studied or where you come from but surely
+    concerned about what you can do on the job. For them, the most important thing is your thought process and your
+    mindset to look into and handle problems. For these counts, candidates are generally scared of the design
+    interviews. but despite all this, I believe there is no need of scaring off. you need to get into what the companies
+    want to know about you during these 40 minutes, which is basically "your approach and strategy to handle a problem"
+    and how organized, disciplined, systematic and professional you are at solving it. What is your capacity to analyze
+    an issue and your level of professional mechanics to solve it step by step?
+
+    In short, system design interview is, just understanding it from interviewer's perspective. During the whole
+    process, it is your discussion with the interviewer that is of core important.
+
+
+"""
