f22dd703213945119d477fb938863dbaeee8e9ee
Randy Li <yunpeng-li@hotmail.com>
2018-05-15 00:35:53 -0700

    the conntents for design part

---
+++ system_design/contents.py
@@ -0,0 +1,144 @@
+"""
+part four: design
+
+an overview of part four
+
+9. designing databases
+    learning objectives
+    introduction
+    database design
+        the process of database design
+        deliverable and outcomes
+        the relational database model
+        well-structured relations
+    normalization
+        rules of normalization
+        functional dependence and primary keys
+        second normal form
+        third normal form
+    transforming e-r diagrams into relations
+        represent entities
+        represent relationships
+        summary of transforming e-r diagrams to relations
+    merging relations
+        an example of merging relations
+        view integration problems
+    logical database design for hoosier burger
+    physical file and database design
+        designing fields
+        choosing data types
+        controlling data integrity
+        designing physical tables
+        arranging table rows
+        designing controls for files
+    physical database design for hoosier burger
+    electronic commerce application: designing databases
+        designing database for pine valley furniture's webstore
+    summary
+    key terms
+    review questions
+    problems and exercises
+    fields exercises
+    references
+
+    bec case: designing databases
+    case questions
+
+10. designing forms and reports
+    learning objectives
+    introduction
+    designing forms and reports
+        the process of designing forms and reports
+        deliverable and outcomes
+    formatting forms and reports
+        general formatting guidelines
+        highlighting information
+        color versus no color
+        displaying text
+        designing tables and lists
+        paper versus electronic reports
+    assessing usability
+        usability success factors
+        measures of usability
+    electronic commerce applications: designing forms and reports for pine valley furniture's webstore
+        general guidelines
+        designing forms and reports at pine valley furniture
+        lightweight graphics
+        forms and data integrity rules
+        stylesheet-ased html
+    summary
+    key terms
+    review questions
+    problems and exercises
+    fields exercises
+    references
+
+    bec case: designing forms and reports
+    case questions
+
+11. designing interfaces and dialogs
+    learning objectives
+    introduction
+    designing interfaces and dialogues
+        the process of designing interfaces and dialogues
+        deliverable and outcomes
+    interaction methods and devices
+        methods of interacting
+        hardware options for system interaction
+    designing interfaces
+        designing layouts
+        structuring data entry
+        controlling data input
+        providing feedback
+        providing help
+    designing dialogues
+        designing the dialogue sequence
+        building prototypes and assessing usability
+    designing interfaces and dialogues in graphical environments
+        graphical interface design issues
+        dialogue design issues in a graphical environment
+    electronic commerce application: designing interfaces and dialogues for pine valley furniture's webstore
+        general guideline
+        designing interfaces and dialogues at pine valley Furniture
+        menu-Driven navigation with cookie crumbs
+    summary
+    key terms
+    review questions
+    problems and exercises
+    field exercises
+    references
+    bec case: designing interfaces and dialogues
+    case questions
+
+12. designing distributed and internet systems
+    learning objectives
+    introduction
+    designing distributed and internet systems
+        the process of designing distributed and internet systems
+        deliverable and outcomes
+    designing lan and client /server systems
+        designing systems for lans
+        designing systems for a client/server architecture
+    could computing
+        what is could computing
+        managing the cloud
+        service-oriented architecture
+        web services
+    designing internet systems
+        internet design fundamentals
+        site consistency
+        design issues related to site management
+    electronic commerce application: designing a distributed advertisement server for pine valley furniture's webstore
+        advertising on pine valley furniture's webstore
+        designing the advertising component
+        designing the management reporting component
+    summary
+    key terms
+    review questions
+    problems and exercises
+    field exercises
+    references
+
+    bec case: designing distributed and internet systems
+    case questions
+"""
