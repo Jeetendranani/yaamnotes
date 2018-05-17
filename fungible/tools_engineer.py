5e71fc4a2ea7e3dda8639aa2748c5d8dcf3346aa
Randy Li <yunpeng-li@hotmail.com>
2018-05-16 18:35:58 -0700

    fungible tools engineer

---
+++ fungible/tools_engineer.py
@@ -0,0 +1,26 @@
+"""
+Central to our mission is a highly programmable chip. your role will be to design and implement a broad range of tools
+that will enable developers and customers to gain meaningful insights into their chip.
+
+Skill, Education, and Experience Required
+
+- MS in computer science or equivalent degree
+- Experience developing debugging, performance analysis, or related tools todo
+- Experience developing tools other have used
+- Experience developing tools with a web UI
+- Ability to write correct C, C++, Java or Python code quickly
+- Architectural sense
+- Self motivated, independent and proactive
+
+- Experience interacting with hardware
+- REST-API design and implementation todo
+- Startup experience
+
+Keywords
+tools,
+javascript, todo
+linux, todo
+socket, todo
+json, todo
+python
+"""
