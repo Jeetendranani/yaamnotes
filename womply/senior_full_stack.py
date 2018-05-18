ef422eab2a1e3a5d4d5be09eb990b1d8d5c4fdb3
Randy Li <yunpeng-li@hotmail.com>
2018-05-18 14:09:58 -0700

    add womply

---
+++ womply/senior_full_stack.py
@@ -0,0 +1,31 @@
+"""
+front-end
+back-end
+
+semantic html todo
+responsive css todo
+
+javascript todo
+
+angularJS todo
+
+React todo
+
+Ruby todo
+
+Rails todo
+Express todo
+Django todo
+
+Java todo
+
+SQL todo
+
+RESTful api todo
+
+AWS todo
+Microservice todo
+Docker todo
+
+CI/CD todo
+"""
