6c81af620a0ea62961e85debbea42dce468ab9b9
Randy Li <yunpeng-li@hotmail.com>
2018-05-18 14:27:11 -0700

    semantic html and responsive css

--- womply/senior_full_stack.py
+++ womply/senior_full_stack.py
@@ -2,8 +2,37 @@
 front-end
 back-end
 
-semantic html todo
-responsive css todo
+semantic html
+    What is semantic html
+
+    Semantic html or semantic markup is html that introduces meaning to the web page rather than just presentation. For
+    example, a <p> tag indicates that the enclosed text is a paragraph. This is both semantic and presentational,
+    because people know what paragraphs are and browsers know how to display them.
+
+    On the flip sides of this equation, tags like <b> and <i> are not semantic, because they define only how the text
+    should look (bold and italic) and do not provide any additional meaning to the markup.
+
+    examples of semantic html tags include the header tags <h1> through <h6>, <blockquote>, <code> and <em>. There are
+    many more semantic HTML tags that can be used as you build a standards-compliant website.
+
+responsive css
+    What is responsive Web Design?
+
+    Responsive web design makes your web page look good on all devices.
+
+    Responsive web design use only html and css.
+
+    Responsive web design is not a program or a javascript.
+
+    Designing for the best experience for all users.
+
+    Web pages can be viewed using many different devices: desktops, tablets, and phones. your web page should look good,
+    and be easy to use, regardless of the device.
+
+    Web pages should not leave out information to fit small devices, but rather adapt its content to fit any device:
+
+    It is called responsive web design when you use css and html to resize, hide, shrink, enlarge, or move the content
+    to make it look good on any screen.
 
 javascript todo
 
