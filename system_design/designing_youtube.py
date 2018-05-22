74dfbcb7a04ec92a6b125330a5ace8298839de3b
Randy Li <yunpeng-li@hotmail.com>
2018-05-22 16:07:33 -0700

    designing youtube

--- system_design/designing_youtube.py
+++ system_design/designing_youtube.py
@@ -218,7 +218,104 @@ Difficulty Level: Medium
 
     This approach as coupe of issues:
 
-        1. What if a user becomes popular?
+        1. What if a user becomes popular? there could be a lot of queries on the server holding that user, creating a
+        performance bottleneck. this will affect the overall performance of our service.
 
+        2. Over time, some users can end up storing a lot of videos compared to other. maintaining a uniform
+        distribution of growing user's data is quite diffecult.
 
+    The recover from these situations either we have to repartition /redistribution our data or user consistent hashing
+    to balance the load between servers.
+
+    Sharding bases on videoid: Our hash function will map each videoid to random server where we will store that video's
+    metadata. To find videos of a user we will query all server, and each server willreturn a set of videos. A
+    centralized server will aggregate and rank these results before returning them to hte user. The approach solver our
+    problem of popular user but shifts it to popular videos.
+
+    We can further improve our performance by introducing cache to store hot videos in front of the database servers.
+
+9. Video Deduplication
+
+    With a huge number of users, uploading a massive amount of video data, our service will have to deal with widespread
+    video duplication. Duplicate videos often differ in aspect ratios or encodings, can contains overlays or additional
+    borders, or can be expects form a longer, original video. the proliferation of duplicate videos can have an impact
+    on many levels:
+
+        1. Data storage: We could be wasting storage space by keeping multiple copies of the same video.
+        2. Caching: duplicate videos would result in degraded cache efficiency by taking up space that could be used
+        for unique content.
+        3. Network usage: Increasing the amount of dta that must be send over the network to in-network caching systems.
+        4. Energy consumption: Higher storage, inefficient cache, and network usage will result in energy wastage.
+
+    For the end user, these inefficiencies will be realized in the form duplicate search results, longer video startup
+    times, and interrupted streaming.
+
+    for our service, deduplication makes most sense early, when a user is uploading a video; as compared to
+    post-processing it to find duplicate videos later. Inline deduplication will save us a lot of resources that can be
+    used to encode, transfer and store the duplicate copy of the video. As soon as any user starts uploading a video,
+    our service can run video matching algorithms (e.g., Block Matching, Phase correlation, etc.) to find duplications,
+
+    Block Matching todo
+    Phase correlation todo
+
+    if we already have a copy of the video being uploaded, we can either stop the upload and use the existing copy or
+    the newly uploaded video if ti is of higher quality. If the newly uploaded video is a subpart of an existing video
+    or vice versa, we can intelligently divide the video into small chunks, so that we only upload those parts that are
+    missing.
+
+10. Load Balancing
+
+    We should use consistent hashing among our cache servers, which will also help in balancing the load between cache
+    servers. Since we will be using a static hash-based scheme to map videos to hostnames, it can lead to uneven load
+    on the logical replicas due to hte different popularity for each video. For instance, if a video becomes popular,
+    the logical replica corresponding to that video will experience more traffic than other servers. These uneven loads
+    for logical replicas can then translate into uneven load distribution on corresponding physical servers. To resolve
+    this issue, any busy server in one location can redirect a client to a less busy server in the same cache location.
+    We can use dynamic http redirection for this scenario.
+
+    dynamic http redirection todo
+
+    however, the use of redirections also has its drawbacks. First, since our service tries to load balance locally, it
+    leads to multiple redirections if the host that receives the redirection can't serve the video. Also each
+    redirection requires a client to make an additional http request; it also leads to higher delays before the video
+    starts playing back. Moreover, intertier (or cross data-center) redirections lead a client to a distant cache
+    location because the higher tier caches are only present at a small number of locations.
+
+11. Cache
+
+    To serve globally distributed users, our service needs a massive-scale video delivery system. Our service should
+    push its content closer to the user using a large number of geographically distributed video cache servers. We need
+    to have a strategy that would maximize user performance and also evenly distributes the load on its cache server.
+
+    We can introduce a cache for metadata servers to cache hot database rows, using memcache to cache the data and
+    application servers before hitting database can quickly check if the cache has the desired rows. Least recently
+    used (LRU) can be a reasonable cache eviction policy for our system. Under this policy, we discard the least
+    recently viewed row first.
+
+    how can we build more intelligent cache? If we go with 80-20 rule, i.e., 20% of daily read volume for video is
+    generating 80% of traffic, meaning that certain videos are so popular that the majority of people view them; It
+    follows that we can try caching 20% of daily read volume of videos and metadata.
+
+12. Content delivery network (CDN)
+
+    A CDN is a system of distributed servers that deliver web content to a user based on the geographic locations of
+    teh user, the origin of the web page and a content delivery server. Take a look at 'CDN' section in our caching
+    chapter.
+
+    CDN todo
+
+    Our service can move most popular videos to CDNs:
+
+        - CDNs replicate content in multiple places. There's a better chance of videos being closer to the user and with
+        fewer hops, videos will stream from a friendlier network.
+
+        - CDN machines make heavy use of caching and can mostly serve videos out of memory.
+
+    Less popular videos (1-20 views per day) that are not cached by CDNs can be served by our servers in various data
+    centers.
+
+13. Fault Tolerance
+
+    We should use consistent hashing for distribution among database servers. Consistent hashing will not only help in
+    replacing a dead server but also helpl in distributing load among servers.
 """
