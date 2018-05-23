6de39093a232f00f99510df1752dcb59402eec80
Randy Li <yunpeng-li@hotmail.com>
2018-05-23 11:51:45 -0700

    thunder token qa position

---
+++ thunder_token/qa_developer.py
@@ -0,0 +1,11 @@
+"""
+- Build the QA team for testing Thunder token.
+- Design and construct test cases to trigger different kinds of abnormal behavior in the thunder protocol
+- python
+- go
+
+The first version of thunder protocol is implemented in Go and so the tests should ideally run in Go.
+
+Go todo
+protocol testing todo
+"""
