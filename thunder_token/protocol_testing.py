efa377b88a64adea235fcbb83c45c6ae983366d4
Randy Li <yunpeng-li@hotmail.com>
2018-05-23 13:22:59 -0700

    protocol testing

---
+++ thunder_token/protocol_testing.py
@@ -0,0 +1,23 @@
+"""
+What is protocol testing?
+
+    Protocol testing is a generic term used by companies working in the communication industry for testing different
+    protocols in domains of switching, wireless, vopi, routing, switching, etc.
+
+What is protocol in a software industry?
+
+    When computer communicates with each other, there is a common set of rules and conditions that each computer has to
+    follow. In other words, protocols determine how data are transmitted between computing devices and over networks.
+
+    protocols are classified into two categories routed protocols and a routing protocols.
+
+    Routed protocols: Routed protocols can be used to send the user data from one network to another network. It carries
+    user traffic like e-mails, web-traffic, file transfers, etc. Routed protocols are ip, ipx, and appletalk.
+
+    Routing protocols: routing protocols are network protocols that determine roues for routrs. It is only used between
+    routers. For example RIP, IGRP, EIGRP, etc.
+
+    In simple terms, router is like a bus used for transportation while routing protocols are signals on the road.
+
+    Based on the type of communication different protocols are used.
+"""
