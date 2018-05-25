24116458642e107020cff5716e944b916386985f
Randy Li <yunpeng-li@hotmail.com>
2018-05-24 17:41:11 -0700

    designing facebook newsfeed

--- system_design/designing_facebook_newsfeed.py
+++ system_design/designing_facebook_newsfeed.py
@@ -243,5 +243,72 @@ Difficulty level: hard
 
     b. Feed publishing
 
+        The process of pushing a post to all the followers is called fanout. By analogy, the push approach is called
+        fanout on write, while the pull approach is called fanout on load. let's discuss different options of
+        publishing data to users.
 
+        1. "pull" model or fan out on load: This method involves keeping all the recent feed data in memory so that
+        users can pull it from the server whenever they need it. Clients can pull the feed data on a regular bases or
+        manually whenever they need it. Possible problems with this approach are
+
+            a. New data might not be shown to the user until they issue a pull request
+            b. it's hard to find the right pull cadence, as most of the time pull requests will result in an empty
+            response if there is no new data causing waste of resources.
+
+        2. "Push" model or fan out on write: for a push system, once a user has published a post, we can immediately
+        push this post to all her followers. The advantage is that when fetching feed, you dont' need to go through
+        your friends list and get feeds for each of them. It significantly reduces read operations. To efficiently
+        manage this, users have to maintain a Long poll request with the server fro receiving the updates. A possible
+        problem with this approach is that when a user has millions of followers (or a celebrity-user), the server has
+        to push updates to a lot of people.
+
+        3. hybrid: Another interesting approach to handle feed data could be to use a hybrid approach, i.e., to do a
+        combination of fan out on write and fan out on load. Specifically, we can stoop pushing posts from users with
+        a high number of followers (a celebrity user) and only push data for those users who have a few hundred (or
+        thousand) followers. For celebrity users, we can let the followers pull teh updates, since the push operation
+        can be extremely costly for users who have a lot of friends or followers therefore, by disabling fanout for
+        them, we can save a huge number of resources. Another alternate approach could be that once a user publishes a
+        post; we can limit the fanout to only her online friends. Also, to get benefits of both the approaches, a
+        combination of push to notify and pull for serving end users is a great way to go. Purely push or pull model is
+        less versatile.
+
+    How many feed items can we return to the client in each request? We should have a maximum limit for the number of
+    items a user can fetch in one request (say 20). But we should let clients choose to specify how many feeds items
+    they want with each request, as the user may like to fetch a different number of posts depending on the device
+    (mobile vs desktop).
+
+    should we always notify users if there are new posts available for their newsfeed? It could be useful for users to
+    get notified whenever new data is available. however, on mobile devices, where data usage is relatively expensive,
+    it can consume unnecessary bandwidth. hence, at least for mobile devices, we can choose not to push data, instead
+    let users 'pull to refresh' to get new posts.
+
+8. Feed ranking
+
+    The most straightforward way to rank posts in a newsfeed is by the creation time of the posts. But today's ranking
+    algorithms are doing a lot more than that to ensure 'important' posts are ranked higher. the high-level idea of
+    ranking is to first select key 'signals' that make a post important and then figure out how to combine them to
+    calculate a final ranking score.
+
+    More specifically, we can select features that are relevant to the importance of any feed item, e.g. number of
+    likes, comments, shares, time of the update, whether the post has images /videos, etc., and then, a score can be
+    calculated using these features. This is generally enough for a simple ranking system. A better ranking system
+    can significantly improve itself by constantly evaluating if we are making progress in user stickiness, retention,
+    ads revenue, etc.
+
+9. Data partitioning
+
+    a. Sharding posts and metadata
+
+        Since we have huge number of new posts every day and our read load is extremely high too, w need to distribute
+        our data onto multiple machines such that we can read / write it efficiently. For sharding our databases that
+        are storing posts and their metadata, we can have a similar design as discussed under Designing Twitter.
+
+    b. Sharding feed data
+
+        For feed data, which is being stored in memory, we can partition it based on UserID. We can try storing all the
+        data of a user on one server. When storing, we can pass the userid to our hash function that will map the user
+        to a cache server where we will store the user's feed objects. Also, for any given user, since we dont' expect
+        to store more than 500 feeditemids, we wouldn't run into a scenario where feed data for a user doesn't fit on
+        a single server. To get the feed of a user, we would always have to query only one server. For future growth and
+        replication, we must use consistent hashing.
 """
