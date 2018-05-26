1255c675a0951d5048aa33e75d347d7d6a39e1d3
Randy Li <yunpeng-li@hotmail.com>
2018-05-25 23:28:27 -0700

    developer career

---
+++ developer_career_guide/contents.py
@@ -0,0 +1,86 @@
+"""
+Contents
+
+Preface
+is this book for me?
+chapter 1 - how to use this book?
+
+section 1
+getting start as a software developer
+
+chapter 2 - how to get start
+chapter 3 - the technical skill you need to have
+chapter 4 - how to develop technical skills
+chapter 5 - what programming language should i learn
+chapter 6 - learning your first programming language
+chapter 7 - going to college
+chapter 8 - coding boot camps
+chapter 9 - teaching yourself
+
+section 2
+getting a job
+
+chapter 10 - internships
+chapter 11 - getting a job without experience
+chapter 12 - how to find a job
+chapter 13 - creating a resume
+chapter 14 - the interview process
+chapter 15 - salaries and negotiation
+chapter 16 - how to leave a job
+chapter 17 - switching mid-carer (late entry)
+chapter 18 - going from qa or another technical role to development
+chapter 19 - contracting vs salary
+chapter 20 - how the recruiting industry works
+
+section 3
+what you need to know about software development
+
+chapter 21 - overview of programming languages
+chapter 22 - what is web development?
+chapter 23 - mobile development
+chapter 24 - back-end development
+chapter 25 - career in video game development
+chapter 26 - dbas and devops
+chapter 27 - software development methodologies
+chapter 28 - testing and qa basics
+chapter 29 - test driven development and unit testing
+chapter 30 - source control
+chapter 31 - continuous integration
+chapter 32 - debugging
+chapter 33 - maintaining code
+chapter 34 - jobs and job titles
+chapter 35 - types of work
+
+section 4
+working as a developer
+
+chapter 36 - dealing with coworkers
+chapter 37 - dealing with your boss
+chapter 38 - working with qa
+chapter 39 - work /life balance
+chapter 40 - working on a team
+chapter 41 - selling your ideas
+chapter 42 - how to dress
+chapter 43 - acing the review process
+chapter 44 - dealing with prejudice
+chapter 45 - being in a leadership position
+chapter 46 - getting a raise or promotioin
+chapter 47 - women in tech
+
+section 5
+advancing your career
+
+chapter 48 - creating a reputation
+chapter 49 - networking and groups
+chapter 50 - keeping you skill up to date
+chapter 51 - generalist vs specialist
+chapter 52 - speaking and conferences
+chapter 53 - creating a blog
+chapter 54 - freelancing and starting a business
+chapter 55 - career paths
+chapter 56 - job stability and security
+chapter 57 - training and certifications
+chapter 58 - side projects
+chapter 59 - Best books to read
+chapter 60 - parting works
+"""
---
+++ developer_career_guide/how_to_start.py
@@ -0,0 +1,14 @@
+"""
+Software development isn't just programming.
+
+The idea behind most software development projects is to automate a manual process, or to create a new automated wa to
+do something that was too difficult to do manually.
+
+You have to know how to manually do something before you can automate it.
+
+The process of software development always begins by first understanding the problem to be solved.
+
+code complete todo
+
+clean code todo
+"""
